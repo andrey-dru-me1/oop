9e609eeb7e5fdbf0d0004b6d102531260f3d7fdb
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-24 20:37:05 +0700

    Javadocs for every class and method have been added.

--- task_1_1_1/src/main/java/BinHeap.java
+++ task_1_1_1/src/main/java/BinHeap.java
@@ -2,28 +2,66 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * BinHeap class represents the binary heap variable type.
+ * The class has inner variable contains binary heap itself
+ * and some methods to work with the one.
+ */
 public class BinHeap {
 
+    /**
+     * The variable contains the binary heap (which is a binary tree) in a list.
+     * <p>It has invariant that every son is always more or equal to his parent.</p>
+     */
     private final List<Integer> binHeap;
 
+    /**
+     * Constructor of the BinHeap class initializes inner variable and sets it to the empty list.
+     */
     public BinHeap() {
         binHeap = new ArrayList<>();
     }
 
+    /**
+     * Returns the element at the specified position of the binary heap.
+     *
+     * @param i Index of the element to return
+     * @return The element at the specified position of the binary heap
+     * @throws IndexOutOfBoundsException – if the index is out of range (index < 0 || index >= size())
+     */
     public Integer get(int i) {
         return binHeap.get(i);
     }
 
+    /**
+     * Returns the number of elements in this list. If this list contains more than Integer.MAX_VALUE elements, returns Integer.MAX_VALUE.
+     *
+     * @return the number of elements in this list
+     */
     public Integer size() {
         return binHeap.size();
     }
 
+    /**
+     * The siftUp function moves the element under index i up to
+     * the root of the binary tree binHeap saving the invariant that
+     * every son is more or equal to his parent.
+     * <p>The whole route of the i-th element becomes a subtree of the one.</p>
+     *
+     * @param i The index of the element to sift up
+     */
     private void siftUp(int i) {
         for (int j = i; j > 0 && this.get(j) < this.get((j - 1) / 2); j = (j - 1) / 2) {
             Collections.swap(binHeap, j, (j - 1) / 2);
         }
     }
 
+    /**
+     * The siftDown function moves the root element down the binary tree
+     * saving invariant that every son is greater or equal to his parent
+     * and that every leaf in the tree has no less depth than the whole
+     * tree height minus one.
+     */
     private void siftDown() {
         Collections.swap(binHeap, 0, binHeap.size() - 1); //swaps root and the last element
         binHeap.remove(binHeap.size() - 1);            //removes the last elem which used to be root
@@ -45,11 +83,22 @@ public class BinHeap {
         }
     }
 
+    /**
+     * Add the value to the tree using siftUp
+     *
+     * @param value Value that will add to the tree
+     */
     public void add(int value) {
         binHeap.add(value);
         siftUp(binHeap.size() - 1);
     }
 
+    /**
+     * Returns the minimum in the binary tree, removes it from the tree
+     * and remake the tree without its root element.
+     *
+     * @return The minimum of the elements in the binary tree
+     */
     public int pop() {
         int result = binHeap.get(0);
         this.siftDown();
--- task_1_1_1/src/main/java/Main.java
+++ task_1_1_1/src/main/java/Main.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 
+/**
+ * Consists only start method main. Scans the unsorted array
+ * from user, sorts it by heapsort and prints the result.
+ */
 public class Main {
 
+    /**
+     * Scans the unsorted array from user, sorts it by heapsort
+     * and prints the result.
+     *
+     * @param args Arguments from the prompt. Not used in program.
+     */
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);            //Creates helper var to scan input System.in
