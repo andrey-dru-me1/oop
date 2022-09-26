77aafef3b623ed950233d4864a84f846eea67cbf
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-26 17:47:04 +0700

    Fixed imports order in MainTest to please reviewdog

--- Task_1_1_1/src/test/java/MainTest.java
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -1,11 +1,10 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class MainTest {
 
