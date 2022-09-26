9c2bb1a9c7f0e2dc40830460a8108c608ab97bf4
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-26 13:05:21 +0700

    Fourth try of reviewdog pleasing

--- Task_1_1_1/src/test/java/MainTest.java
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -1,7 +1,7 @@
-import org.junit.jupiter.api.Assertions;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
