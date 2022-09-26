b19012949e58be3d07833c765ed226f243705f03
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-26 13:18:08 +0700

    The script has been parametrized.

--- Task_1_1_1/src/main/java/bash_script.bat
+++ Task_1_1_1/src/main/java/bash_script.bat
@@ -1,3 +1,3 @@
-javadoc Main.java BinHeap.java
-javac Main.java BinHeap.java
-java Main 3 2 1 4 5
+IF %~1==javadoc javadoc Main.java BinHeap.java
+IF %~1==javac javac Main.java BinHeap.java
+IF %~1==java java Main 3 2 1 4 5
