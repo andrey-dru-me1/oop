5de71ae2f6ea40f5be76922ad12992223c79f96b
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-28 00:22:40 +0700

    Inner variable has been made private; Javadocs have been created.

--- Task_1_2_1/src/main/java/Stack.java
+++ Task_1_2_1/src/main/java/Stack.java
@@ -1,30 +1,55 @@
 import java.util.Arrays;
 
+/**
+ * A black box where you can only put elements on its top (push)
+ * or extract them from the top with pop.
+ */
 public class Stack {
 
-    Object[] stack;
+    private Object[] stack;
 
     public Stack() {
         stack = new Object[0];
     }
 
+    /**
+     * Appends element to the ecd of stack.
+     *
+     * @param value The value to add to the stack
+     */
     public void push(Object value) {
         stack = Arrays.copyOf(stack, stack.length + 1);
         stack[stack.length - 1] = value;
     }
 
+    /**
+     * Appends the array to the end of stack.
+     *
+     * @param arr Array of elements to append in the stack
+     */
     public void pushStack(Object[] arr) {
         int prevLen = stack.length;
         stack = Arrays.copyOf(stack, stack.length + arr.length);
         System.arraycopy(arr, 0, stack, prevLen, stack.length - prevLen);
     }
 
+    /**
+     * Returns the latest element and removes the one from the stack.
+     *
+     * @return The latest element
+     */
     public Object pop() {
         Object res = stack[stack.length - 1];
         stack = Arrays.copyOf(stack, stack.length - 1);
         return res;
     }
 
+    /**
+     * Returns an array of the cnt elements from the end of stack.
+     *
+     * @param cnt Count of elements to pop
+     * @return Array of extracted elements
+     */
     public Object[] popStack(int cnt) {
         Object[] arr = new Object[cnt];
         System.arraycopy(stack, stack.length - cnt, arr, 0, cnt);
@@ -32,6 +57,11 @@ public class Stack {
         return arr;
     }
 
+    /**
+     * Returns count of elements in the stack.
+     *
+     * @return Count of elements
+     */
     public int count() {
         return stack.length;
     }
