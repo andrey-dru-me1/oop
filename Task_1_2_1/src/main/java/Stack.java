5f32b369f39a5c70a2529b43e81a734f44467fd5
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-27 21:20:11 +0700

    Stack type has been made universal

--- Task_1_2_1/src/main/java/Stack.java
+++ Task_1_2_1/src/main/java/Stack.java
@@ -2,31 +2,31 @@ import java.util.Arrays;
 
 public class Stack {
 
-    int[] stack;
+    Object[] stack;
 
     public Stack() {
-        stack = new int[0];
+        stack = new Object[0];
     }
 
-    public void push(int value) {
+    public void push(Object value) {
         stack = Arrays.copyOf(stack, stack.length + 1);
         stack[stack.length - 1] = value;
     }
 
-    public void pushStack(int[] arr) {
+    public void pushStack(Object[] arr) {
         int prevLen = stack.length;
         stack = Arrays.copyOf(stack, stack.length + arr.length);
         System.arraycopy(arr, 0, stack, prevLen, stack.length - prevLen);
     }
 
-    public int pop() {
-        int res = stack[stack.length - 1];
+    public Object pop() {
+        Object res = stack[stack.length - 1];
         stack = Arrays.copyOf(stack, stack.length - 1);
         return res;
     }
 
-    public int[] popStack(int cnt) {
-        int[] arr = new int[cnt];
+    public Object[] popStack(int cnt) {
+        Object[] arr = new Object[cnt];
         System.arraycopy(stack, stack.length - cnt, arr, 0, cnt);
         stack = Arrays.copyOf(stack, stack.length - cnt);
         return arr;
