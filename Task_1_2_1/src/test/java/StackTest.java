a95128c0f4f320a3bef723a15eedffbb7aef2ce4
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-27 23:45:53 +0700

    Another one .....

---
+++ Task_1_2_1/src/test/java/StackTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class StackTest {
+
+    @Test
+    void test() {
+        Stack stack = new Stack();
+        stack.push(2);
+        stack.push(7);
+        stack.pushStack(new Integer[]{4, 8});
+        Assertions.assertEquals(stack.pop(), 8);
+        Assertions.assertEquals(Arrays.toString(stack.popStack(2)).trim(), "[7, 4]");
+        Assertions.assertEquals(stack.count(), 1);
+    }
+
+}
