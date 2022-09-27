a690d6e4a0fa77d98d2ba5d8eb8440813b7186e9
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-27 22:36:58 +0700

    Parameterized test has been added.

--- Task_1_1_1/src/test/java/MainTest.java
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -1,51 +1,59 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
 
 class MainTest {
 
-    @Test
-    void testExample() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(Arrays.asList(3, 2, 1, 4, 5))),
-                new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))
+    private static Stream<Arguments> methodForTests() {
+        return Stream.of(
+                Arguments.of(
+                        new ArrayList<>(Arrays.asList(3, 2, 1, 4, 5)),
+                        new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))
+                ),
+                Arguments.of(
+                        new ArrayList<>(List.of()),
+                        new ArrayList<>(List.of())
+                ),
+                Arguments.of(
+                        new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0)),
+                        new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0))
+                ),
+                Arguments.of(
+                        new ArrayList<>(Arrays.asList(3, -2, 1, 4, -5)),
+                        new ArrayList<>(Arrays.asList(-5, -2, 1, 3, 4))
+                ),
+                Arguments.of(
+                        new ArrayList<>(Arrays.asList(354154616, 2, 354154636, -4, -35456831)),
+                        new ArrayList<>(Arrays.asList(-35456831, -4, 2, 354154616, 354154636))
+                ),
+                Arguments.of(
+                        new ArrayList<>(Arrays.asList(Integer.MAX_VALUE, Integer.MIN_VALUE, -4, 5, 0)),
+                        new ArrayList<>(Arrays.asList(Integer.MIN_VALUE, -4, 0, 5, Integer.MAX_VALUE))
+                ),
+                Arguments.of(
+                        new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3)),
+                        new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3))
+                ),
+                Arguments.of(
+                        new ArrayList<>(Arrays.asList(3, 2, 1, 0, -1, -2, -3)),
+                        new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3))
+                )
         );
     }
 
-    @Test
-    void testVoidArray() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(List.of())),
-                new ArrayList<>(List.of())
-        );
-    }
-
-    @Test
-    void testConstEverywhere() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0))),
-                new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0))
-        );
-    }
-
-    @Test
-    void testWithNegNums() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(Arrays.asList(3, -2, 1, 4, -5))),
-                new ArrayList<>(Arrays.asList(-5, -2, 1, 3, 4))
-        );
-    }
-
-    @Test
-    void testBigNums() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(Arrays.asList(354154616, 2, 354154636, -4, -35456831))),
-                new ArrayList<>(Arrays.asList(-35456831, -4, 2, 354154616, 354154636))
-        );
+    @ParameterizedTest
+    @MethodSource("methodForTests")
+    void testExample(ArrayList<Integer> input, ArrayList<Integer> expected) {
+        Assertions.assertEquals(Main.sort(input), expected);
     }
 
     @Test
@@ -56,31 +64,5 @@ class MainTest {
         Main.main(new String[]{"3", "2", "1", "4", "5"});
 
         Assertions.assertEquals(out.toString().trim(), "[1, 2, 3, 4, 5]");
-    }
-
-    @Test
-    void testMaxInt() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(
-                        Arrays.asList(Integer.MAX_VALUE, Integer.MIN_VALUE, -4, 5, 0))
-                ),
-                new ArrayList<>(Arrays.asList(Integer.MIN_VALUE, -4, 0, 5, Integer.MAX_VALUE))
-        );
-    }
-
-    @Test
-    void testStraight() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3))),
-                new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3))
-        );
-    }
-
-    @Test
-    void testReverse() {
-        Assertions.assertEquals(
-                Main.sort(new ArrayList<>(Arrays.asList(3, 2, 1, 0, -1, -2, -3))),
-                new ArrayList<>(Arrays.asList(-3, -2, -1, 0, 1, 2, 3))
-        );
     }
 }
