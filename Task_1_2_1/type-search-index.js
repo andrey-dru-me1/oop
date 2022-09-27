cd6433ec1d0996691f5a7332bca3403beebd2ce9
andrey-dru-me1 <andrey-dru-me1@users.noreply.github.com>
2022-09-27 17:09:20 +0000

    Deploying to gh-pages from @ andrey-dru-me1/oop@ff7450dbfb08ee7a2ae68426edb87a2a0b4819e4 🚀

--- Task_1_2_1/type-search-index.js
+++ Task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Stack"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Stack"}]
