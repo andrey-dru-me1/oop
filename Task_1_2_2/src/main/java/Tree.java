422c2df1039583d44df2c9aeeb43d85c0b07ab0e
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-29 11:57:00 +0700

    add(), remove() and toString() operations have been added.

--- Task_1_2_2/src/main/java/Tree.java
+++ Task_1_2_2/src/main/java/Tree.java
@@ -2,27 +2,59 @@ import java.util.Arrays;
 
 public class Tree<T> {
 
-    Node<T> root;
+    private final Node<T> root;
 
     public Tree() {
-        root = new Node<>(null);
+        root = new Node<>(null, null);
     }
 
     public static class Node<T> {
 
         T value;
         Node<T>[] children;
+        Node<T> parent;
 
         @SuppressWarnings("unchecked")
-        public Node(T value) {
+        public Node(Node<T> parent, T value) {
             this.value = value;
-            children = new Tree.Node[0];
+            children = new Node[0];
+            this.parent = parent;
         }
 
         public Node<T> add(T value) {
             children = Arrays.copyOf(children, children.length + 1);
-            children[children.length - 1] = new Node<>(value);
+            children[children.length - 1] = new Node<>(this, value);
             return children[children.length - 1];
+        }
+
+        public void remove() {
+            for (Node<T> i : children) {
+                i.remove();
+            }
+
+            if (parent == null) return;
+
+            for (int i = 0; i < parent.children.length; i++) {
+                if (parent.children[i] == this) {
+                    System.arraycopy(parent.children, i + 1, parent.children, i, parent.children.length - i - 1);
+                    parent.children = Arrays.copyOf(parent.children, parent.children.length - 1);
+                }
+            }
+
+        }
+
+        public String toString() {
+            StringBuilder res = new StringBuilder("( " + value + ", [");
+            for (int i = 0; i < children.length; i++) {
+
+                res.append(children[i].toString());
+
+                if (i < children.length - 1) {
+                    res.append(", ");
+                }
+            }
+            res.append("] )");
+            return res.toString();
         }
 
     }
@@ -33,6 +65,18 @@ public class Tree<T> {
 
     public Node<T> add(Node<T> node, T value) {
         return node.add(value);
+    }
+
+    public void remove() {
+        root.remove();
+    }
+
+    public void remove(Node<T> node) {
+        node.remove();
+    }
+
+    public String toString() {
+        return root.toString();
     }
 
 }
--- Task_1_2_2/src/test/java/TreeTest.java
+++ Task_1_2_2/src/test/java/TreeTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TreeTest {
@@ -6,8 +7,14 @@ class TreeTest {
     void test() {
         Tree<Integer> tree = new Tree<>();
         Tree.Node<Integer> seven = tree.add(7);
-        tree.add(seven, 8);
+        Tree.Node<Integer> eight = tree.add(seven, 8);
+        Assertions.assertEquals(tree.toString().trim(), "( null, [( 7, [( 8, [] )] )] )");
+
         seven.add(9);
+        Assertions.assertEquals(tree.toString().trim(), "( null, [( 7, [( 8, [] ), ( 9, [] )] )] )");
+
+        tree.remove(eight);
+        Assertions.assertEquals(tree.toString().trim(), "( null, [( 7, [( 9, [] )] )] )");
     }
 
 }
