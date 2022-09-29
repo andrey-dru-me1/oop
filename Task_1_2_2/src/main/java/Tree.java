f938fec7d0d6e3a2c9767f7ba5605e6450089732
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-09-29 10:49:23 +0700

    First draft

--- Task_1_2_2/src/main/java/Tree.java
+++ Task_1_2_2/src/main/java/Tree.java
@@ -1,13 +1,38 @@
+import java.util.Arrays;
+
 public class Tree<T> {
 
     Node<T> root;
 
     public Tree() {
-        root = new Node<>();
+        root = new Node<>(null);
     }
 
-    private class Node<T> {
+    public static class Node<T> {
 
+        T value;
+        Node<T>[] children;
+
+        @SuppressWarnings("unchecked")
+        public Node(T value) {
+            this.value = value;
+            children = new Tree.Node[0];
+        }
+
+        public Node<T> add(T value) {
+            children = Arrays.copyOf(children, children.length + 1);
+            children[children.length - 1] = new Node<>(value);
+            return children[children.length - 1];
+        }
+
+    }
+
+    public Node<T> add(T value) {
+        return root.add(value);
+    }
+
+    public Node<T> add(Node<T> node, T value) {
+        return node.add(value);
     }
 
 }
