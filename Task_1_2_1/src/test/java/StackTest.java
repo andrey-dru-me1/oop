ea938bf7c64f628a4e63005ec78d4317ffa7f8af
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-10-01 09:50:37 +0700

    More tests for the tests' God!

--- Task_1_2_1/src/test/java/StackTest.java
+++ Task_1_2_1/src/test/java/StackTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 class StackTest {
 
@@ -15,5 +16,59 @@ class StackTest {
         Assertions.assertEquals(Arrays.toString(stack.popStack(2)).trim(), "[7, 4]");
         Assertions.assertEquals(stack.count(), 1);
     }
+
+    @Test
+    void testStrings() {
+        Stack<String> stack = new Stack<>();
+        stack.pushStack(new String[]{"abrakadabra", "nothing", "Is", "HeRe"});
+        stack.push("");
+        Assertions.assertArrayEquals(stack.popStack(3), new String[]{"Is", "HeRe", ""});
+        Assertions.assertEquals(stack.count(), 2);
+        Assertions.assertEquals(stack.pop(), "nothing");
+    }
+
+    @Test
+    void testNewObject() {
+
+        class MyObj {
+
+            final Integer num;
+            final String[] strings;
+            final Double dnum;
+
+            MyObj(Integer num, String[] strings, Double dnum) {
+                this.num = num;
+                this.strings = strings;
+                this.dnum = dnum;
+            }
+
+            @Override
+            public boolean equals(Object inObj) {
+                if (!(inObj instanceof MyObj obj)) return false;
+                return (Arrays.equals(this.strings, obj.strings)) &&
+                        (Objects.equals(Double.valueOf(this.num), obj.dnum)) &&
+                        (Objects.equals(Double.valueOf(obj.num), this.dnum));
+            }
+        }
+
+        MyObj obj1 = new MyObj(17, new String[]{"I", "like", "formalin"}, 27.0);
+        MyObj obj2 = new MyObj(17, new String[]{"I", "like", "formalin"}, 27.0);
+        MyObj obj3 = new MyObj(17, new String[]{"I", "hate", "formalin"}, 27.0);
+        MyObj obj4 = new MyObj(27, new String[]{"I", "like", "formalin"}, 17.0);
+
+        Stack<MyObj> stack = new Stack<>();
+        stack.pushStack(new MyObj[]{obj1, obj2, obj3}); //1, 2, 3
+
+        Assertions.assertEquals(stack.count(), 3);
+        Assertions.assertNotEquals(stack.pop(), obj4);  //1, 2
+        Assertions.assertEquals(stack.count(), 2);
+
+        stack.push(obj4);   //1, 2, 4
+        Assertions.assertEquals(stack.count(), 3);
+        Assertions.assertArrayEquals(stack.popStack(2), new MyObj[]{obj4, obj2});   //1
+        Assertions.assertEquals(stack.count(), 1);
+
+    }
+
 
 }
