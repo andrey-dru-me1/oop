2ae677571118557f1207f6da091fce774770a491
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-10-12 11:37:02 +0700

    All the implemented methods are created. (Small issue fix)

--- Task_1_2_2/src/main/java/ru/nsu/fit/melnikov/task_1_2_2/Tree.java
+++ Task_1_2_2/src/main/java/ru/nsu/fit/melnikov/task_1_2_2/Tree.java
@@ -1,9 +1,6 @@
 package ru.nsu.fit.melnikov.task_1_2_2;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Represents a tree type. Has only root and its descendants.
@@ -25,7 +22,7 @@ public class Tree<T> implements Collection<T> {
      */
     public class Node {
 
-        T value;
+        Object value;
         List<Node> children;
         Node parent;
 
@@ -41,10 +38,11 @@ public class Tree<T> implements Collection<T> {
          * @param value The value that will be bound on the new element.
          * @return The new node.
          */
-        public boolean add(T value) {
+        public Node add(T value) {
             Node newNode = new Node(this, value);
+            children.add(newNode);
             Tree.this.size++;
-            return children.add(newNode);
+            return newNode;
         }
 
         /**
@@ -57,6 +55,7 @@ public class Tree<T> implements Collection<T> {
 
             if (parent == null) return true;
 
+            Tree.this.size--;
             return parent.children.remove(this);
 
         }
@@ -89,34 +88,31 @@ public class Tree<T> implements Collection<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        boolean res = false;
+        for (T i : this) {
+            if (i == o) {
+                res = true;
+                break;
+            }
+        }
+        return res;
     }
 
     @Override
     public Iterator<T> iterator() {
-        List<Node> Q = new ArrayList<>();
-        Q.add(root);
-        for (Node i : Q) {
-            Q.addAll(i.children);
-        }
-        Q.remove(0);
-
-        List<T> q = new ArrayList<>();
-        for (Node i : Q) {
-            q.add(i.value);
-        }
-        return q.iterator();
+        return this.toList().iterator();
     }
 
+    @SuppressWarnings("unchecked")
     public Iterator<T> iteratorDFS() {
         class HelperClass extends ArrayList<Node> {
             public void foo(Node node) {
-                this.addAll(node.children);
+                this.add(node);
                 for (Node i : node.children) {
                     foo(i);
                 }
@@ -125,57 +121,110 @@ public class Tree<T> implements Collection<T> {
 
         HelperClass Q = new HelperClass();
         Q.foo(root);
+        Q.remove(0);
 
         List<T> q = new ArrayList<>();
         for (Node i : Q) {
-            q.add(i.value);
+            q.add((T) i.value);
         }
         return q.iterator();
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return this.toList().toArray();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<T> toList() {
+
+        List<Node> Q = toNodeList();
+
+        List<T> q = new ArrayList<>();
+        for (Node i : Q) {
+            q.add((T) i.value);
+        }
+        return q;
+    }
+
+    private List<Node> toNodeList() {
+        List<Node> Q = new ArrayList<>();
+        Q.add(root);
+        for (Node i : Q) {
+            Q.addAll(i.children);
+        }
+        Q.remove(0);
+        return Q;
     }
 
     @Override
-    public boolean add(Object o) {
-        return false;
+    public boolean add(T o) {
+        root.add(o);
+        return true;
     }
 
     @Override
-    public boolean remove(Object o) {
-        return false;
+    public boolean remove(Object o) throws ClassCastException {
+        if (o.getClass() != Node.class) {
+            throw new ClassCastException("Expected 'Node' class but found '" + o.getClass().toString() + "'.");
+        }
+        @SuppressWarnings("unchecked") boolean res = ((Node) o).remove();
+        return res;
     }
 
     @Override
-    public boolean addAll(Collection c) {
-        return false;
+    public boolean addAll(Collection<? extends T> c) {
+        for (T i : c) {
+            this.add(i);
+        }
+        return true;
     }
 
     @Override
     public void clear() {
-
+        root.remove();
     }
 
     @Override
     public boolean retainAll(Collection c) {
-        return false;
+        for (Node i : this.toNodeList()) {
+            if (!c.contains(i)) {
+                i.remove();
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        return false;
+        for (Object i : c) {
+            this.remove(i);
+        }
+        return true;
     }
 
     @Override
     public boolean containsAll(Collection c) {
-        return false;
+        boolean res = true;
+        for (Object i : c) {
+            if (!contains(i)) {
+                res = false;
+                break;
+            }
+        }
+        return res;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Object[] toArray(Object[] a) {
-        return new Object[0];
+        if (a.length < size)
+            // Make a new array of a's runtime type, but my contents:
+            return Arrays.copyOf(this.toArray(), size, a.getClass());
+        System.arraycopy(this.toArray(), 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
     }
 
     public String toString() {
