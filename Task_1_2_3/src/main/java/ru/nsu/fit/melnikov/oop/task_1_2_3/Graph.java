dead9e6c22b1f10bb33c68066ef46ee52712e111
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-10-24 18:47:53 +0700

    Id is replaced with values of edges and vertices

--- Task_1_2_3/src/main/java/ru/nsu/fit/melnikov/oop/task_1_2_3/Graph.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/melnikov/oop/task_1_2_3/Graph.java
@@ -1,250 +1,284 @@
 package ru.nsu.fit.melnikov.oop.task_1_2_3;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.*;
 
-public class Graph<T> {
+public class Graph<V, E> {
 
-    private final List<Vertex> vertices;
+    private final List<Vert> verts;
     private final List<Edge> edges;
 
-    private int vids;
-    private int eids;
-
     public Graph() {
-        this.vertices = new ArrayList<>();
+        this.verts = new ArrayList<>();
         this.edges = new ArrayList<>();
-        vids = 0;
-        eids = 0;
     }
 
-    private class Vertex {
+    private class Vert {
 
-        private T value;
+        private V val;
         private final Set<Edge> incEdges;
-        private final int id;
 
-        private Vertex(T value) {
-            this.value = value;
+        private Vert(V val) {
+            this.val = val;
             incEdges = new HashSet<>();
-            this.id = vids++;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public T getValue() {
-            return value;
+            verts.add(this);
         }
 
         public Set<Edge> getIncEdges() {
-            return incEdges;
+            return new HashSet<>(incEdges);
         }
 
-        public T setValue(T value) {
-            T prevVal = this.value;
-            this.value = value;
+        public V getVal() {
+            return this.val;
+        }
+
+        public V setVal(V val) {
+            V prevVal = this.val;
+            this.val = val;
             return prevVal;
         }
 
+        public V remove() {
+            List<Edge> toRemove = new ArrayList<>(this.incEdges);
+            for (Edge e : toRemove) {
+                e.remove();
+            }
+            verts.remove(this);
+            return this.val;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean equals(Object o) {
+            if (!(o instanceof Graph.Vert)) {
+                return false;
+            }
+            return ((Vert) o).val.equals(this.val);
+        }
+
         public void addEdge(Edge edge) {
-            incEdges.add(edge);
+            this.incEdges.add(edge);
         }
 
         public void addAllEdges(Collection<Edge> edges) {
-            for (Edge edge : edges) {
-                this.addEdge(edge);
-            }
+            this.incEdges.addAll(edges);
+        }
+
+        public boolean removeE(Edge edge) {
+            return incEdges.remove(edge);
         }
 
     }
 
     private class Edge {
 
-        private Double weight;
-        private Vertex from;
-        private Vertex to;
-        private final int id;
+        private Double w;
+        private Vert from;
+        private Vert to;
+        private E val;
 
-        private Edge(Vertex from, Vertex to, Double weight) {
+        private Edge(Vert from, Vert to, Double w, E val) {
             this.from = from;
             this.to = to;
-            this.weight = weight;
-            this.id = eids++;
+            this.w = w;
+            this.val = val;
             from.incEdges.add(this);
             to.incEdges.add(this);
+            edges.add(this);
         }
 
-        public int getId() {
-            return id;
+        public E getVal() {
+            return this.val;
         }
 
-        public Double getWeight() {
-            return weight;
+        public Double getW() {
+            return w;
         }
 
-        public Vertex getFromVert() {
+        public Vert getVFrom() {
             return from;
         }
 
-        public Vertex getToVert() {
+        public Vert getVTo() {
             return to;
         }
 
-        public List<Vertex> getVertices() {
-            return Arrays.asList(from, to);
+        public E setVal(E val) {
+            E prevVal = this.val;
+            this.val = val;
+            return prevVal;
         }
 
-        public Double setWeight(Double weight) {
-            Double prevW = this.weight;
-            this.weight = weight;
+        public Double setW(Double w) {
+            Double prevW = this.w;
+            this.w = w;
             return prevW;
         }
 
-        public void setFrom(Vertex vertex) {
-            this.from = vertex;
+        public void setVFrom(Vert v) {
+            this.from = v;
         }
 
-        public void setTo(Vertex to) {
+        public void setVTo(Vert v) {
+            this.to = v;
+        }
+
+        public void setVerts(Vert from, Vert to) {
+            this.from = from;
             this.to = to;
         }
 
-    }
-
-    public int addVertex(T value) {
-        Vertex v = new Vertex(value);
-        vertices.add(v);
-        return v.getId();
-    }
-
-    public List<Integer> addVertices(T[] values) {
-        List<Integer> l = new ArrayList<>();
-        for (T i : values) {
-            l.add(addVertex(i));
+        public E remove() throws NoSuchElementException {
+            from.removeE(this);
+            to.removeE(this);
+            edges.remove(this);
+            return this.val;
         }
-        return l;
-    }
 
-    public int addEdge(int v_from_id, int v_to_id, Double weight) {
-        Edge e = new Edge(
-                getVertexById(v_from_id),
-                getVertexById(v_to_id),
-                weight
-        );
-        edges.add(e);
-        return e.getId();
-    }
-
-    public void removeEdge(int id) {
-        Edge e = getEdgeById(id);
-        e.from.getIncEdges().remove(e);
-        e.to.getIncEdges().remove(e);
-        edges.remove(e);
-    }
-
-    public T removeVertex(int id) {
-        Vertex v = getVertexById(id);
-        T val = v.getValue();
-
-        List<Integer> toRemove = new ArrayList<>();
-        for (Edge e : v.getIncEdges()) {
-            toRemove.add(e.getId());
-        }
-        toRemove.sort((x, y) -> y - x);
-        for (int i : toRemove) {
-            removeEdge(i);
-        }
-        vertices.remove(v);
-        return val;
-    }
-
-    public T setVertexValue(int id, T value) {
-        return getVertexById(id).setValue(value);
-    }
-
-    public T getVertexValue(int id) {
-        return getVertexById(id).getValue();
-    }
-
-    public List<Integer> getVerticesWithValue(T value) {
-        List<Integer> l = new ArrayList<>();
-        for (Vertex i : this.vertices) {
-            if (i.getValue().equals(value)) {
-                l.add(i.getId());
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean equals(Object o) {
+            if (!(o instanceof Graph.Edge)) {
+                return false;
             }
+            Edge e = (Edge) o;
+            return (
+                    e.w.equals(this.w) &&
+                            e.from.equals(this.from) &&
+                            e.to.equals(this.to) &&
+                            e.val.equals(this.val)
+            );
         }
-        return l;
+
     }
 
-    public List<Integer> getEdgesByVerts(int v_from_id, int v_to_id) {
-        List<Integer> l = new ArrayList<>();
-        for (Edge i : this.edges) {
-            if (i.getFromVert().getId() == v_from_id && i.getToVert().getId() == v_to_id) {
-                l.add(i.getId());
-            }
-        }
-        return l;
-    }
-
-    private @NotNull Vertex getVertexById(int id) throws NoSuchElementException {
-        for (Vertex v : vertices) {
-            if (v.getId() == id) {
+    private Vert getV(V val) throws NoSuchElementException {
+        for (Vert v : verts) {
+            if (v.getVal().equals(val)) {
                 return v;
             }
         }
         throw new NoSuchElementException();
     }
 
-    public Double setEdgeWeight(int id, Double weight) {
-        return getEdgeById(id).setWeight(weight);
-    }
-
-    public void setEdgeInceds(int e_id, int v_from_id, int v_to_id) {
-        Edge e = getEdgeById(e_id);
-        Vertex vf = getVertexById(v_from_id);
-        Vertex vt = getVertexById(v_to_id);
-        Vertex prevVf = e.getFromVert();
-        Vertex prevVt = e.getToVert();
-        if (prevVf != vf) {
-            prevVf.getIncEdges().remove(e);
-            vf.getIncEdges().add(e);
+    public boolean containsV(V val) {
+        try {
+            getV(val);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
         }
-        if (prevVt != vt) {
-            prevVt.getIncEdges().remove(e);
-            vt.getIncEdges().add(e);
-        }
-        e.setFrom(vf);
-        e.setTo(vt);
     }
 
-    public int getEdgeFrom(int id) {
-        return getEdgeById(id).getFromVert().getId();
-    }
-
-    public int getEdgeTo(int id) {
-        return getEdgeById(id).getToVert().getId();
-    }
-
-    public Double getEdgeWeight(int id) {
-        return getEdgeById(id).getWeight();
-    }
-
-    private @NotNull Edge getEdgeById(int id) throws NoSuchElementException {
+    private Edge getE(E val) throws NoSuchElementException {
         for (Edge e : edges) {
-            if (e.getId() == id) {
+            if (e.getVal().equals(val)) {
                 return e;
             }
         }
         throw new NoSuchElementException();
     }
 
-    public int VerticesCount() {
-        return vertices.size();
+    public boolean containsE(E val) {
+        try {
+            getE(val);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
 
-    public int EdgesCount() {
+    public boolean addV(V val) {
+        if (containsV(val)) {
+            return false;
+        }
+        Vert v = new Vert(val);
+        return true;
+    }
+
+    public List<Boolean> addVerts(V[] vals) {
+        List<Boolean> res = new ArrayList<>();
+        for (V val : vals) {
+            res.add(addV(val));
+        }
+        return res;
+    }
+
+    public boolean addE(V vFrom, V vTo, Double w, E val) {
+
+        Vert from = getV(vFrom);
+        Vert to = getV(vTo);
+
+        Edge e;
+        try {
+            e = getE(val);
+        } catch (NoSuchElementException exc) {
+            e = new Edge(from, to, w, val);
+            return true;
+        }
+        e.setVFrom(from);
+        e.setVTo(to);
+        e.setW(w);
+        return false;
+    }
+
+    public E removeE(E val) throws NoSuchElementException {
+        return getE(val).remove();
+    }
+
+    public V removeV(V val) {
+        return getV(val).remove();
+    }
+
+    public void setVVal(V prevVal, V newVal) {
+        Vert v = getV(prevVal);
+        v.setVal(newVal);
+    }
+
+    public Double setEdgeW(E val, Double w) {
+        return getE(val).setW(w);
+    }
+
+    public void setEVal(E prevVal, E newVal) {
+        getE(prevVal).setVal(newVal);
+    }
+
+    public void setEInceds(E val, V vFrom, V vTo) {
+        Edge e = getE(val);
+        Vert from = getV(vFrom);
+        Vert to = getV(vTo);
+        e.setVerts(from, to);
+    }
+
+    public V getEFrom(E val) {
+        return getE(val).getVFrom().getVal();
+    }
+
+    public V getETo(E val) {
+        return getE(val).getVTo().getVal();
+    }
+
+    public Double getEdgeW(E val) {
+        return getE(val).getW();
+    }
+
+    public List<E> getEdgesByVerts(V vFrom, V vTo) {
+        Vert from = getV(vFrom);
+        Vert to = getV(vTo);
+
+        List<E> res = new ArrayList<>();
+        for (Edge e : from.getIncEdges()) {
+            if (e.getVTo().equals(to)) {
+                res.add(e.getVal());
+            }
+        }
+        return res;
+    }
+
+    public int VCnt() {
+        return verts.size();
+    }
+
+    public int ECnt() {
         return edges.size();
     }
 
