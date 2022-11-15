91214bf4c13a96a56383e7b5d925b1ac909ef926
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-11-16 00:25:40 +0700

    Second algorithm of substring search is implemented

--- Task_1_3_1/src/main/java/ru/nsu/oop/melnikov/substringsearch/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/oop/melnikov/substringsearch/SubstringSearch.java
@@ -3,6 +3,7 @@ package ru.nsu.oop.melnikov.substringsearch;
 import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Class with some substring searching algorithms.
@@ -58,6 +59,57 @@ public class SubstringSearch {
         }
         filePos -= substring.length();
         return filePos;
+    }
+
+    /**
+     * Algorithm of substring search using Z-function.
+     *
+     * @param reader    stream of bytes to search in
+     * @param substring substring to search
+     * @return index of byte where substring begins
+     * @throws IOException when there is no such substring in reader stream
+     */
+    public static int searchZ(BufferedReader reader, String substring) throws IOException {
+
+        char[] buf = new char[1024];    //buffer size is set to 1024 bytes
+        Arrays.fill(buf, (char) 0);
+        if (reader.read(buf, 0, substring.length()) == -1) {
+            throw new EOFException();
+        }
+        int offset = 0;
+        while (reader.read(buf, substring.length(), 1024 - substring.length()) != -1) {
+
+            //Z-function implementation
+            Integer[] z = new Integer[1024];
+            Arrays.fill(z, 0);
+            int left = 0;
+            int right = 0;
+            for (int i = 1; i < 1024 && buf[i] != 0; i++) {
+                if (i <= right) {
+                    z[i] = Math.min(right - i + 1, z[i - left]);
+                }
+                while (
+                        i + z[i] < 1024
+                                && substring.charAt(z[i]) == buf[i + z[i]]
+                ) {
+                    z[i]++;
+                    if (z[i] == substring.length()) {
+                        return offset + i;
+                    }
+                }
+                if (i + z[i] - 1 > right) {
+                    left = i;
+                    right = i + z[i] - 1;
+                }
+            }
+
+            System.arraycopy(buf, 1024 - substring.length(), buf, 0, substring.length());
+            offset += 1024 - substring.length();
+
+        }
+
+        throw new EOFException();
+
     }
 
 }
