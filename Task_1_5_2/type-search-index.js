172e6348bf7ed5a0e65c436e5679d9250163e9b8
andrey-dru-me1 <andrey-dru-me1@users.noreply.github.com>
2022-12-19 16:04:38 +0000

    Deploying to gh-pages from @ andrey-dru-me1/oop@e4db16ffe140bede7abb8ceb0f54e0c6a53e62b0 🚀

---
+++ Task_1_5_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.fit.oop.melnikov.notebook","l":"Notebook"},{"p":"ru.nsu.fit.oop.melnikov.notebook","l":"Record"}];updateSearchResults();
