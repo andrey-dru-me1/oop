d0dee81aa7b05ad24f8868a66fd16ab6374ab8cf
andrey-dru-me1 <andrey-dru-me1@users.noreply.github.com>
2022-12-22 04:49:33 +0000

    Deploying to gh-pages from @ andrey-dru-me1/oop@57555fa358d08b93e3a326e68f95e8bcaaa19da7 🚀

--- Task_1_5_1/package-search-index.js
+++ Task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.oop.melnikov.calculator"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.oop.melnikov.calculator"},{"l":"ru.nsu.fit.oop.melnikov.calculator.operations"}];updateSearchResults();
