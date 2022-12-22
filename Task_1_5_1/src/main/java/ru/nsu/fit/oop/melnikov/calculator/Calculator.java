7556308e12e9be55e94d4fcee254626ee9ac4409
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-12-22 14:46:40 +0700

    add javadocs

--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/melnikov/calculator/Calculator.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/melnikov/calculator/Calculator.java
@@ -5,14 +5,28 @@ import ru.nsu.fit.oop.melnikov.calculator.operations.*;
 
 import java.util.*;
 
+/**
+ * Implementation of calculator with settable and extendable set of supported operations.
+ */
 public class Calculator {
 
+    /**
+     * Set of supported operations.
+     */
     private final Map<String, Operation> operations;
 
     public Calculator(Map<String, Operation> operations) {
         this.operations = operations;
     }
 
+    /**
+     * Parses an input expression.
+     *
+     * @param scanner scanner that have string to parse
+     * @return result of input string expression
+     * @throws Operation.WrongOperandsCountException when arity of operation does not
+     *                                               match the count of operands in string
+     */
     private Double parseAtom(@NotNull Scanner scanner) throws Operation.WrongOperandsCountException {
 
         String buf;
@@ -35,6 +49,13 @@ public class Calculator {
         return operation.apply(operands);
     }
 
+    /**
+     * Loads all basic functions and constants in calculator and execute it.
+     *
+     * @param args aren't expected and aren't handled in this implementation.
+     * @throws Operation.WrongOperandsCountException when arity of operation does not
+     *                                               match the count of operands in string
+     */
     public static void main(String[] args) throws Operation.WrongOperandsCountException {
         Calculator calculator = new Calculator(Map.ofEntries(
 
--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/melnikov/calculator/operations/Operation.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/melnikov/calculator/operations/Operation.java
@@ -2,12 +2,32 @@ package ru.nsu.fit.oop.melnikov.calculator.operations;
 
 import java.util.List;
 
+/**
+ * Super-class for each operation that can be supported by calculator.
+ */
 public abstract class Operation {
 
+    /**
+     * returns arity of operation.
+     *
+     * @return arity of operation
+     */
     abstract public int getArity();
 
+    /**
+     * Calculates a result of function.
+     *
+     * @param operands list of operands to pass in operation function
+     * @return result of calculation
+     */
     abstract protected Double calculate(List<Double> operands);
 
+    /**
+     * Applies current operation to operands.
+     *
+     * @param operands list of operands to pass in operation function
+     * @return result of calculation
+     */
     public Double apply(List<Double> operands) throws WrongOperandsCountException {
         if (operands.size() != getArity()) throw new WrongOperandsCountException();
         return calculate(operands);
