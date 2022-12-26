c0bf1fcab091824c3121a8af60ea8f7afd9a7ce3
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-12-26 17:07:32 +0700

    add javadocs for clone and parse

--- Task_1_5_1/src/main/java/ru/nsu/fit/oop/melnikov/calculator/operations/Operation.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/oop/melnikov/calculator/operations/Operation.java
@@ -36,8 +36,18 @@ public abstract class Operation {
         return calculate(operands);
     }
 
+    /**
+     * Clones current object and returns a clone.
+     *
+     * @return new object which is the same as this object
+     */
     public abstract Operation clone();
 
+    /**
+     * @param string string to parse (single word)
+     * @return null, if operation not intended for parsing input string,
+     * and parsed operation object otherwise
+     */
     public abstract Operation parse(String string);
 
     static public class WrongOperandsCountException extends RuntimeException {
