f604cbdc5cc05a060d8fb8f1e9c40fe83b868fe5
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-12-27 23:12:34 +0700

    replace custom argument parsing with picocli

--- Task_1_5_2/src/main/java/ru/nsu/fit/oop/melnikov/notebook/Main.java
+++ Task_1_5_2/src/main/java/ru/nsu/fit/oop/melnikov/notebook/Main.java
@@ -3,6 +3,7 @@ package ru.nsu.fit.oop.melnikov.notebook;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.jetbrains.annotations.NotNull;
+import picocli.CommandLine;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,45 +15,49 @@ import java.util.Arrays;
 import java.util.Locale;
 
 public class Main {
+    private Notebook notebook;
+
+    @CommandLine.Parameters(index = "0")
+    private void setNotebook(String fileName) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        String fullFileName = fileName + ".json";
+
+        try {
+            notebook = mapper.readValue(new File(fullFileName), Notebook.class);
+        } catch (IOException e) {
+            notebook = new Notebook(fullFileName);
+        }
+    }
+
+    @CommandLine.Option(names = {"-add"}, arity = "2")
+    private void addNote(String[] noteInfo) {
+        System.out.println("Add record");
+        notebook.addRecord(noteInfo[0], noteInfo[1]);
+    }
+
+    @CommandLine.Option(names = {"-rm"}, arity = "1")
+    private void removeNote(String noteName) {
+        System.out.println("Remove record");
+        notebook.removeRecord(noteName);
+    }
+
+    @CommandLine.Option(names = {"-show"}, arity = "0..")
+    private void showAllNotes(String[] data) {
+        if (data.length < 2) {
+            System.out.println(notebook);
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.y HH:mm", Locale.ENGLISH).withZone(ZoneId.systemDefault());
+            Instant from = LocalDateTime.parse(data[0], formatter).atZone(ZoneId.systemDefault()).toInstant();
+            Instant to = LocalDateTime.parse(data[1], formatter).atZone(ZoneId.systemDefault()).toInstant();
+            System.out.print(notebook.showWithTimeLimits(formatter, from, to, Arrays.stream(data).skip(2).toArray(String[]::new)));
+        }
+    }
 
     public static void main(String @NotNull [] args) {
-        if (args.length > 1) {
 
-            final String FILE_PATH = args[0] + ".json";
+        CommandLine.populateCommand(new Main(), args);
 
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.registerModule(new JavaTimeModule());
-
-            Notebook notebook;
-
-            try {
-                notebook = mapper.readValue(new File(FILE_PATH), Notebook.class);
-            } catch (IOException e) {
-                notebook = new Notebook(FILE_PATH);
-            }
-
-            switch (args[1]) {
-                case "-add" -> {
-                    System.out.println("Add record");
-                    notebook.addRecord(args[2], args[3]);
-                }
-                case "-rm" -> {
-                    System.out.println("Remove record");
-                    notebook.removeRecord(args[2]);
-                }
-                case "-show" -> {
-                    if (args.length < 4) {
-                        System.out.println(notebook);
-                    } else {
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.y HH:mm", Locale.ENGLISH).withZone(ZoneId.systemDefault());
-                        Instant from = LocalDateTime.parse(args[2], formatter).atZone(ZoneId.systemDefault()).toInstant();
-                        Instant to = LocalDateTime.parse(args[3], formatter).atZone(ZoneId.systemDefault()).toInstant();
-                        System.out.print(notebook.showWithTimeLimits(formatter, from, to, Arrays.stream(args).skip(4).toArray(String[]::new)));
-
-                    }
-                }
-            }
-        } else
-            System.out.println("usage: notebook [parameter]\n\tparameters:\n\t\t-add\n\t\t--rm\n\t\t-show");
     }
 }
