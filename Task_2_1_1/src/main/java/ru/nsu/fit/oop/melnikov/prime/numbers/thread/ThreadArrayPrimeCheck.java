89a9275a7873f76ecdc9ebe632606f1b7e25d734
andrey <a.melnikov4@g.nsu.ru>
2023-02-28 15:06:37 +0700

    remove extra spacing

--- Task_2_1_1/src/main/java/ru/nsu/fit/oop/melnikov/prime/numbers/thread/ThreadArrayPrimeCheck.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/oop/melnikov/prime/numbers/thread/ThreadArrayPrimeCheck.java
@@ -10,65 +10,65 @@ public class ThreadArrayPrimeCheck {
     /**
      * Searches a non-prime number from an array and breaks if found.
      */
-        private static class PrimeCheck extends Thread {
+    private static class PrimeCheck extends Thread {
 
-            private final int[] numbers;
-            private final CommonVars res;
+        private final int[] numbers;
+        private final CommonVars res;
 
-            public PrimeCheck(int[] numbers, CommonVars res) {
-                this.numbers = numbers;
-                this.res = res;
-            }
+        public PrimeCheck(int[] numbers, CommonVars res) {
+            this.numbers = numbers;
+            this.res = res;
+        }
 
-            @Override
-            public void run() {
+        @Override
+        public void run() {
 
-                int index;
+            int index;
 
-                while (!res.hasCompositeNumber() && (index = res.getIndexAndIncrement()) < numbers.length) {
+            while (!res.hasCompositeNumber() && (index = res.getIndexAndIncrement()) < numbers.length) {
 
-                    int number = numbers[index];
+                int number = numbers[index];
 
-                    for (int divider = 2; divider * divider <= number && !res.hasCompositeNumber(); divider++) {
-                        if (number % divider == 0) {
-                            res.setCompositeNumber();
-                            break;
-                        }
+                for (int divider = 2; divider * divider <= number && !res.hasCompositeNumber(); divider++) {
+                    if (number % divider == 0) {
+                        res.setCompositeNumber();
+                        break;
                     }
-
                 }
+
+            }
+        }
+    }
+
+    /**
+     * Checks if input array contains non-prime numbers or not.
+     * Parallel execution using threads.
+     *
+     * @param array       array an input array where non-prime numbers will be searched
+     * @param threadCount count of threads to work
+     * @return true if there is at least one non-prime number and false otherwise
+     */
+    public static @NotNull Boolean check(int @NotNull [] array, int threadCount, CommonVars res) {
+
+        Deque<Thread> threads = new ArrayDeque<>(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            PrimeCheck thread = new PrimeCheck(array, res);
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread i : threads) {
+            try {
+                i.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
 
-        /**
-         * Checks if input array contains non-prime numbers or not.
-         * Parallel execution using threads.
-         *
-         * @param array       array an input array where non-prime numbers will be searched
-         * @param threadCount count of threads to work
-         * @return true if there is at least one non-prime number and false otherwise
-         */
-        public static @NotNull Boolean check(int @NotNull [] array, int threadCount, CommonVars res) {
+        return res.hasCompositeNumber();
 
-            Deque<Thread> threads = new ArrayDeque<>(threadCount);
-
-            for (int i = 0; i < threadCount; i++) {
-                PrimeCheck thread = new PrimeCheck(array, res);
-                thread.start();
-                threads.add(thread);
-            }
-
-            for (Thread i : threads) {
-                try {
-                    i.join();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return res.hasCompositeNumber();
-
-        }
+    }
 
 
 }
