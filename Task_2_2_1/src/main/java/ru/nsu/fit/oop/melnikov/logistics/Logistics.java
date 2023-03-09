9767711d035adb34cd8a6816b3e8bbc87f2763eb
andrey <a.melnikov4@g.nsu.ru>
2023-03-09 10:22:17 +0700

    implement all cook's work

--- Task_2_2_1/src/main/java/ru/nsu/fit/oop/melnikov/logistics/Logistics.java
+++ Task_2_2_1/src/main/java/ru/nsu/fit/oop/melnikov/logistics/Logistics.java
@@ -1,85 +1,62 @@
 package ru.nsu.fit.oop.melnikov.logistics;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import ru.nsu.fit.oop.melnikov.kitchen.Cook;
+import ru.nsu.fit.oop.melnikov.warehouse.WareHouse;
 
 public class Logistics {
 
-//  private final WareHouse wareHouse;
-//  private final Queue<Thread> cooksWaitingForOrder;
-//  private final Queue<Thread> couriersWaitingForPizzas;
-//  private volatile int ordersInQueue;
-//  private final Queue<Thread> waitForWareHouseQueue;
-//
-//  public Logistics(Set<Courier> couriers, Set<Cook> cooks, WareHouse wareHouse) {
-//    this.wareHouse = wareHouse;
-//    ordersInQueue = 0;
-//    cooksWaitingForOrder = new ArrayDeque<>();
-//    couriersWaitingForPizzas = new ArrayDeque<>();
-//    waitForWareHouseQueue = new ArrayDeque<>();
-//
-//    for(Cook cook : cooks) {
-//      new Thread(() -> cook.work(
-//          () -> {
-//            synchronized (this) {
-//              if (ordersInQueue > 0) {
-//                ordersInQueue--;
-//                return true;
-//              }
-//              return false;
-//            }
-//          },
-//          () -> cooksWaitingForOrder.add(Thread.currentThread()),
-//          () -> cooksWaitingForOrder.remove(Thread.currentThread()),
-//          wareHouse::putPizza,
-//          () -> waitForWareHouseQueue.add(Thread.currentThread()),
-//          () -> {
-//            waitForWareHouseQueue.remove(Thread.currentThread());
-//            couriersWaitingForPizzas.peek().notify();
-//          }
-//      )).start();
-//    }
-//  }
+  private final int[] orders;
+  private final WareHouse wareHouse;
 
-  private final Queue<Thread> freeCooks;
-  private volatile int orders;
+  public Logistics(@NotNull Set<Cook> cooks, WareHouse wareHouse) {
+    this.wareHouse = wareHouse;
+    this.orders = new int[1];
 
-  public Logistics(@NotNull Set<Cook> cooks) {
-    freeCooks = new ArrayDeque<>();
-    orders = 0;
     for (Cook cook : cooks) {
-      new Thread(() -> cook.work(() -> {
-        while (true) {
-          synchronized (this) {
-            if (orders > 0) {
-              orders--;
-              freeCooks.remove(Thread.currentThread());
-              System.out.println("Cook received the order");
-              return;
-            } else {
-              try {
-                freeCooks.add(Thread.currentThread());
-                wait();
-              } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-              }
-            }
-          }
+      new Thread(() -> cook.work(this::waitForOrders, this::putPizza)).start();
+    }
+
+  }
+
+  private void putPizza() {
+    synchronized (wareHouse) {
+      try {
+        wareHouse.putPizza();
+      } catch (IndexOutOfBoundsException e) {
+        try {
+          wareHouse.wait();
+        } catch (InterruptedException ex) {
+          throw new RuntimeException(ex);
         }
-      })).start();
+      }
     }
   }
 
-  public synchronized void orderNewPizza() {
-    System.out.println("An order has received");
-    if (freeCooks.isEmpty()) {
-      System.out.println("The order is sent to queue");
-      orders++;
-    } else {
-      freeCooks.peek().notify();
+  private void waitForOrders() {
+    while (true) {
+      synchronized (orders) {
+        if (orders[0] > 0) {
+          orders[0]--;
+          System.out.println("Cook received the order");
+          return;
+        } else {
+          try {
+            orders.wait();
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+        }
+      }
+    }
+  }
+
+  public void orderNewPizza() {
+    synchronized (orders) {
+      System.out.println("An order has received");
+      orders[0]++;
+      orders.notify();
     }
   }
 
--- Task_2_2_1/src/test/java/ru/nsu/fit/oop/melnikov/logistics/LogisticsTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/fit/oop/melnikov/logistics/LogisticsTest.java
@@ -6,26 +6,35 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 import ru.nsu.fit.oop.melnikov.kitchen.Cook;
+import ru.nsu.fit.oop.melnikov.warehouse.WareHouse;
 
 class LogisticsTest {
 
   @Test
   void test() {
     Set<Cook> cooks = new HashSet<>();
+    WareHouse wareHouse = new WareHouse(15);
+
     cooks.add(new Cook(1));
-    Logistics logistics = new Logistics(cooks);
+
+    Logistics logistics = new Logistics(cooks, wareHouse);
+
     logistics.orderNewPizza();
+
     try {
-      sleep(1000);
+      sleep(6000);
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
+
     logistics.orderNewPizza();
+
     try {
       sleep(5000);
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
+
   }
 
 }
