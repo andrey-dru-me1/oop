4eeda88be3637b650ef522b25df5990db90506ef
andrey <a.melnikov4@g.nsu.ru>
2023-04-14 16:58:36 +0700

    add first movements for implementing gui

--- Task_2_3_1/src/main/java/ru/nsu/fit/oop/melnikov/game/snake/view/Main.java
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/melnikov/game/snake/view/Main.java
@@ -1,10 +1,12 @@
 package ru.nsu.fit.oop.melnikov.game.snake.view;
 
+import java.util.Objects;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.util.Objects;
 
 public class Main extends Application {
 
@@ -15,7 +17,22 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setTitle("Snake the game");
-    primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("icon.png"))));
+    primaryStage
+        .getIcons()
+        .add(
+            new Image(
+                Objects.requireNonNull(
+                    getClass().getClassLoader().getResourceAsStream("icon.png")
+                )
+            )
+        );
+    primaryStage.setWidth(800);
+    primaryStage.setHeight(600);
     primaryStage.show();
+
+    Label label = new Label("Hello world");
+    Scene scene = new Scene(label);
+    primaryStage.setScene(scene);
+    label.setAlignment(Pos.CENTER);
   }
 }
