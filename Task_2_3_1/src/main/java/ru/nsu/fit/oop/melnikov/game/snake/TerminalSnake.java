94c2ab77cd0bd1841124080b5579cac873482d3f
andrey <a.melnikov4@g.nsu.ru>
2023-05-18 04:50:14 +0700

    implement terminal snake

--- Task_2_3_1/src/main/java/ru/nsu/fit/oop/melnikov/game/snake/TerminalSnake.java
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/melnikov/game/snake/TerminalSnake.java
@@ -1,24 +1,101 @@
 package ru.nsu.fit.oop.melnikov.game.snake;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.TextColor;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.swing.*;
+import ru.nsu.fit.oop.melnikov.game.data.loader.DataLoader;
+import ru.nsu.fit.oop.melnikov.game.snake.model.direction.Direction;
+import ru.nsu.fit.oop.melnikov.game.snake.model.field.Field;
+import ru.nsu.fit.oop.melnikov.game.snake.model.field.cell.Cell;
+import ru.nsu.fit.oop.melnikov.game.snake.model.field.cell.objects.Apple;
+import ru.nsu.fit.oop.melnikov.game.snake.model.field.cell.objects.SnakeNode;
+import ru.nsu.fit.oop.melnikov.game.snake.model.field.cell.objects.Wall;
+import ru.nsu.fit.oop.melnikov.game.snake.model.snake.Snake;
 
 public class TerminalSnake {
 
   public static void main(String[] args) throws IOException {
-    Terminal terminal = new DefaultTerminalFactory().createTerminal();
-    Screen screen = new TerminalScreen(terminal);
-    Panel panel = new Panel();
-    BasicWindow window = new BasicWindow();
-    window.setComponent(panel);
-    MultiWindowTextGUI gui =
-        new MultiWindowTextGUI(
-            screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
-    gui.addWindowAndWait(window);
+    DataLoader dataLoader = new DataLoader("big_map.txt");
+
+    Snake snake = dataLoader.getSnake();
+    Field field = dataLoader.getField();
+    field.generateApple();
+
+    DefaultTerminalFactory defaultTerminalFactory =
+        new DefaultTerminalFactory()
+            .setInitialTerminalSize(new TerminalSize(field.getWidth() * 2, field.getHeight()));
+
+    try (Screen screen = defaultTerminalFactory.createScreen();
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor()) {
+
+      screen.startScreen();
+
+      redraw(screen, field);
+
+      executorService.scheduleAtFixedRate(
+          () -> {
+            try {
+
+              KeyStroke keyStroke = screen.pollInput();
+
+              Direction direction = snake.getDirection();
+              if (keyStroke != null) {
+                switch (keyStroke.getKeyType()) {
+                  case ArrowUp -> direction = Direction.UP;
+                  case ArrowDown -> direction = Direction.DOWN;
+                  case ArrowLeft -> direction = Direction.LEFT;
+                  case ArrowRight -> direction = Direction.RIGHT;
+                }
+              }
+
+              snake.move(direction);
+
+              if (snake.isDestroyed()) {
+                executorService.shutdown();
+              }
+
+              redraw(screen, field);
+
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          },
+          100,
+          100,
+          TimeUnit.MILLISECONDS);
+
+      executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void redraw(Screen screen, Field field) throws IOException {
+    for (Cell[] row : field.getCells()) {
+      for (Cell cell : row) {
+        char cellChar;
+        if (cell.contains(Wall.class)) {
+          cellChar = '#';
+        } else if (cell.contains(SnakeNode.class)) {
+          cellChar = 'o';
+        } else if (cell.contains(Apple.class)) {
+          cellChar = 'a';
+        } else {
+          cellChar = ' ';
+        }
+        TextCharacter textCharacter = new TextCharacter(cellChar);
+        screen.setCharacter(cell.getX() * 2, cell.getY(), textCharacter);
+      }
+    }
+    screen.refresh();
   }
 }
