97433b5a5be4161dc3cdfb4318c6788ddd73070e
andrey <a.melnikov4@g.nsu.ru>
2023-05-30 14:33:42 +0700

    commit

---
+++ Task_2_4_1/dsl_configs/groups.groovy
@@ -0,0 +1,14 @@
+package ru.nsu.fit.oop.melnikov.dsl.config
+
+groups {
+    group(21214) {
+        students {
+            student('andrey-dru-me1') {
+                name = 'Мельников Андрей Петрович'
+            }
+            student('evangelionexpert') {
+                name = 'Смоляков Павел Евгеньевич'
+            }
+        }
+    }
+}
