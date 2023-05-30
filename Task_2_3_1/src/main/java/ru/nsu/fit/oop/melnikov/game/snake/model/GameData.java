76e0dc29f959f6b968d7cb9b2cec3643e9422692
andrey <a.melnikov4@g.nsu.ru>
2023-05-30 10:10:45 +0700

    start implementing GameData

---
+++ Task_2_3_1/src/main/java/ru/nsu/fit/oop/melnikov/game/snake/model/GameData.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.oop.melnikov.game.snake.model;
+
+import ru.nsu.fit.oop.melnikov.game.snake.model.field.Field;
+import ru.nsu.fit.oop.melnikov.game.snake.model.snake.Snake;
+
+public class GameData {
+    private final Field field;
+    private final Snake snake;
+
+}
